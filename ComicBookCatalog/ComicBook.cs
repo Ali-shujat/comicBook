807f8027bc8326798dbe6c4a495e0773e1335c4d
Shujat Ali <shuja4you@hotmail.com>
2020-05-11 15:25:51 +0100

    2nd commit

---
+++ ComicBookCatalog/ComicBook.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ComicBookCatalog
+{
+    public class ComicBook
+    {
+        public int Id;
+        public string Title;
+        public int Volume;
+        public int Issue;
+        public int Year;
+        public int Month;
+        public string Publisher;
+        public string Writer;
+        public string Ink;
+        public string Letterer;
+        public string Colorist;
+        public string Editor;
+        public string EditorInChef;
+        public string Synopsis;
+        public string Story;
+        public int part;
+
+    }
+}
